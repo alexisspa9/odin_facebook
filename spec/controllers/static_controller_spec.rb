102ea5248342345e42f0688419bd3dcb3003ee4b
alexisspa9 <alexisspa9@gmail.com>
2019-04-16 13:17:24 +0300

    add some static controller tests

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe StaticController, type: :controller do
 
+	before(:each) do
+		@u = User.new(email: "user@example.com", name: "Alexander", password: "babacha1234", password_confirmation: "babacha1234")
+	end
+
+	  it "should redirect to login page if not logged in" do
+		get :home
+      	expect(response).to redirect_to new_user_session_path
+	  end
+
+	  it "should redirect to home page if logged in" do
+	  	@u.confirm
+	  	sign_in @u
+		get :home
+      	expect(response).to render_template(:home)
+	  end
   
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'devise'
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -38,7 +39,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include Devise::Test::ControllerHelpers, :type => :controller
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -49,11 +50,13 @@ RSpec.configure do |config|
   #     RSpec.describe UsersController, :type => :controller do
   #       # ...
   #     end
-  #
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
+  config.render_views
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
